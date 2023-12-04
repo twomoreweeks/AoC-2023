fcabef9dbf9c3660374afcafc0ee4e92fcd4c52f
twomoreweeks <57001026+twomoreweeks@users.noreply.github.com>
2023-12-04 07:24:39 +0000

    refactor aoc3_1.py

--- aoc3_1.py
+++ aoc3_1.py
@@ -2,9 +2,8 @@ j=1
 D={}
 for l in open('o'):
   z=D.values()
-  l=l.replace("  ", " ").split(" | ")
-  k=len({*l[0].split(": ")[1].split()}&{*l[1].split()})
-  e=-~sum(v[1]for v in z if(j+1)in v[0])
-  D[j+1]=[i+2 for i in range(j,k+j)],e
+  l=l.split(": ")[1].split(" | ")
+  k=len({*l[0].split()}&{*l[1].split()})
+  D[j]=list(range(j,k+j)),-~sum(v[1]for v in z if j-1 in v[0])
   j+=1
 print(sum(v[1]for v in z))
